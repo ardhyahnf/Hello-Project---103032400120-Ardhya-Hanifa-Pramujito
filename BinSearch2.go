c84409bfd303de41e70fe5bba2911b532d85bd23
ares <arkeiiuc3@gmail.com>
2025-10-29 09:16:21 +0700

    wow keren

--- BinSearch2.go
+++ BinSearch2.go
@@ -1,18 +1,17 @@
+// wow keren
 package main
 
-import "fmt"
-
-func BinSearch2(tab tabInt, n, x int)int{
+func BinSearch2(tab tabInt, n, x int) int {
 	var left, right, mid int
 	var idx int
-	
+
 	left = 0
-	right = n-1
-	idx = -1 
+	right = n - 1
+	idx = -1
 	for left <= right && idx != -1 {
-		mid = (left + right)/2
-		if x < tab[mid]{
-			right = mid - 1 
+		mid = (left + right) / 2
+		if x < tab[mid] {
+			right = mid - 1
 		} else if x > tab[mid] {
 			left = mid + 1
 		} else {
